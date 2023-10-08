ed47ed9359ebe86bd7fd36b21212425d08b73bcd
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-10-08 14:41:49 +0400

    comparison of real numbers

--- Lab1 AISD/images.h
+++ Lab1 AISD/images.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <random>
 #include <time.h>
 
@@ -15,8 +14,11 @@ public:
 	T& operator()(int x, int y);
 	Image& operator+(T a);
 	Image& operator*(T a);
-	Image& operator(Image a);
-
+	Image& operator*(Image a);
+	Image& operator+(Image a);
+	Image& operator!();
+	bool operator==(Image a);
+	double image_koefficient();
 };
 
 template <typename T>
@@ -87,6 +89,80 @@ Image<bool>& Image<bool>::operator*(Image<bool> a){
 	return result;
 }
 
+Image<bool>& Image<bool>::operator+(Image<bool> a) {
+	if (a.size_x != size_x || a.size_y != size_y) {
+		//исключение 
+	}
+	Image<bool>* result = new Image<bool>(size_x, size_y, false);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			result->image[i][j] = (bool)(image[i][j] || a.image[i][j]);
+		}
+	}
+	return *result;
+}
 
+Image<bool>& Image<bool>::operator!() {
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			image[i][j] = !image[i][j];
+		}
+	}
+	return *this;
+}
 
+template <typename T>
+bool Image<T>::operator==(Image<T> a)
+{
+	if (a.size_x != size_x || a.size_y != size_y) {
+		//искл
+	}
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			if (a.image[i][j] != image[i][j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
+double Image<bool>::image_koefficient() {
+	double sum = 0;
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			sum += image[i][j];
+		}
+	}
+	return sum / (size_x * size_y * 1);
+}
+
+double Image<int>::image_koefficient() {
+	double sum = 0;
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			sum += image[i][j];
+		}
+	}
+	return sum / (size_x * size_y * INT_MAX);
+}
+
+double Image<short>::image_koefficient() {
+	double sum = 0;
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			sum += image[i][j];
+		}
+	}
+	return sum / (size_x * size_y * SHRT_MAX);
+}
+
+double Image<double>::image_koefficient() {
+	double sum = 0;
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++) {
+			sum += image[i][j];
+		}
+	}
+	return sum / (size_x * size_y * DBL_MAX);
+}
